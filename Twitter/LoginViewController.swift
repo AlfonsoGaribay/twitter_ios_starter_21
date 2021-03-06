b1a629ca7dc2dc78b7f4e2064d9d98395c97e2b7
Alfonso <alfonso.garibay@sjsu.edu>
2021-03-05 23:11:38 -0800

    this is everything up to getting tweets tutorial, the issue of staying logged in persists

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -18,12 +18,28 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    //!!
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+        
+        
+        
+    }
+    
+    
+    //!!
     @IBAction func onLoginButton(_ sender: Any) {
         print("Button has been pressed")
         
         let myUrl = "https://api.twitter.com/oauth/request_token"
 
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            UserDefaults.standard.set(true,forKey:"userLoggedIn." )
+            
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("could not log in!")
